b03a98abde51d5dd0ba8d20581b0e90c422ed524
anifilm <anifilm02@gmail.com>
2024-03-13 12:20:34 +0900

    init files for neovim config

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,42 @@
+local mapKey = require("utils.keyMapper").mapKey
+
+-- Neotree toggle
+mapKey('<leader>e', ':Neotree toggle<CR>')
+
+-- pane navigation 
+mapKey('<C-h>', '<C-w>h') -- Left
+mapKey('<C-j>', '<C-w>j') -- Down
+mapKey('<C-k>', '<C-w>k') -- Up
+mapKey('<C-l>', '<C-w>l') -- Right
+
+-- clear search highlights 
+mapKey('<leader>h', ':nohlsearch<CR>')
+
+-- set paste
+mapKey('<leader>p', ':set paste noai<CR>')
+
+-- coc.nvim
+local keyset = vim.keymap.set
+-- Autocomplete
+function _G.check_back_space()
+    local col = vim.fn.col('.') - 1
+    return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
+end
+
+-- Use Tab for trigger completion with characters ahead and navigate
+-- NOTE: There's always a completion item selected by default, you may want to enable
+-- no select by setting `"suggest.noselect": true` in your configuration file
+-- NOTE: Use command ':verbose imap <tab>' to make sure Tab is not mapped by
+-- other plugins before putting this into your config
+local opts = {silent = true, noremap = true, expr = true, replace_keycodes = false}
+--keyset("i", "<TAB>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
+--keyset("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
+
+-- Make <CR> to accept selected completion item or notify coc.nvim to format
+-- <C-g>u breaks current undo, please make your own choice
+keyset("i", "<TAB>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
+
+-- Use <c-j> to trigger snippets
+keyset("i", "<c-j>", "<Plug>(coc-snippets-expand-jump)")
+-- Use <c-space> to trigger completion
+keyset("i", "<c-space>", "coc#refresh()", {silent = true, expr = true})
---
+++ readme.md
@@ -0,0 +1,11 @@
+$ sudo apt list neovim (check version >= 8.0)
+$ sudo apt install neovim
+
+~/.config # not included in this repo ()
+    /nvim
+        /lua # init.lua
+            /config  # config .lua files
+            /plugins # plugins here (lazy package manager)
+            /utils # utility/helper .lua files
+
+$ sudo apt install ripgrep
